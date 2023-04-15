707f3377560b2ef08f2a818938112a8b6f344713
Ryann Flores <102124511+RynnFlrs@users.noreply.github.com>
2023-04-15 18:51:11 +0800

    Update asnwer.js

--- task-2.2/asnwer.js
+++ task-2.2/asnwer.js
@@ -1,11 +1,11 @@
 const { readFile, writeFile } = require('node:fs')
 
 function read(filename, cb) {
-  readFile(filename, cb)
+  return readFile(filename, cb)
 }
 
 function write(filename, content) {
-  writeFile(filename, content)
+  return writeFile(filename, content)
 }
 
 function answer(file1, file2, file3) {
